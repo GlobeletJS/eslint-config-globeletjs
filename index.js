b9feeb196a6b48de58c5a3bb6ebbcb8ca7edc3f1
Jeshurun Hembd <jjhembd@gmail.com>
2021-07-05 17:49:18 -0500

    Lint the config

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
+// eslint doesn't allow exporting as an ES module ?!?!
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "space-infix-ops": [
-          "error",
-          { "int32Hint": false }
-        ],
-        "object-curly-spacing": [
-          "error",
-          "always"
-        ],
-        "spaced-comment": [
-          "error",
-          "always"
-        ],
-        "space-before-function-paren": [
-          "error",
-          "never"
-        ],
-        "array-bracket-spacing": [
-          "error",
-          "never"
-        ],
-        "comma-spacing": [
-          "error",
-          { "before": false, "after": true }
-        ],
-        "key-spacing": [
-          "error",
-          { "beforeColon": false, "afterColon": true }
-        ],
-        "keyword-spacing": [
-          "error"
-        ],
-        "no-trailing-spaces": [
-          "error"
-        ],
-        "semi-spacing": [
-          "error"
-        ],
-        "space-before-blocks": [
-          "error",
-          "always"
-        ]
-    }
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'space-infix-ops': [
+      'error',
+      { 'int32Hint': false }
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always'
+    ],
+    'spaced-comment': [
+      'error',
+      'always'
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ],
+    'array-bracket-spacing': [
+      'error',
+      'never'
+    ],
+    'comma-spacing': [
+      'error',
+      { 'before': false, 'after': true }
+    ],
+    'key-spacing': [
+      'error',
+      { 'beforeColon': false, 'afterColon': true }
+    ],
+    'keyword-spacing': [
+      'error'
+    ],
+    'no-trailing-spaces': [
+      'error'
+    ],
+    'semi-spacing': [
+      'error'
+    ],
+    'space-before-blocks': [
+      'error',
+      'always'
+    ]
+  }
 };
